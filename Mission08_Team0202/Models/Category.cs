865bc9b403cffbc50487a5ee9279331869f0c0ba
garrettjash <garrettashcroft@gmail.com>
2024-02-26 19:47:02 -0700

    Db Setup

---
+++ Mission08_Team0202/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mission08_Team0202.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string Category1 { get; set; } = null!;
+}
