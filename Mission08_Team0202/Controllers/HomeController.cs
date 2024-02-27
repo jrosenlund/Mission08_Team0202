52fbd7dc19b440ad2f2950156e513457bde69d08
jrosenlund <119529299+jrosenlund@users.noreply.github.com>
2024-02-26 20:45:58 -0700

    commit

--- Mission08_Team0202/Controllers/HomeController.cs
+++ Mission08_Team0202/Controllers/HomeController.cs
@@ -6,14 +6,39 @@ namespace Mission08_Team0202.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private TaskContext _context; // Database "Instance"
+        public HomeController(TaskContext temp) // Constructor
+        {
+            _context = temp;
+        }
 
         public IActionResult Index()
+        {
+            
+
+            return View(/* Give four objects representing each quadrant (quad1, quad2, quad3, quad4) */);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
         {
             return View();
         }
 
+        [HttpPost]
         public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit()
         {
             return View();
         }
