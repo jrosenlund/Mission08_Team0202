1031e09e1c358a7fcf3729bd7827c1fb6fb824f2
jrosenlund <119529299+jrosenlund@users.noreply.github.com>
2024-02-27 18:15:53 -0700

    added delete post method

--- Mission08_Team0202/Controllers/HomeController.cs
+++ Mission08_Team0202/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Mission08_Team0202.Models;
 using System.Diagnostics;
 
@@ -42,6 +43,24 @@ namespace Mission08_Team0202.Controllers
         //{
         //    return View();
         //}
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            // Identify target record based on task id
+            var taskToDelete = _repo.Tasks.FirstOrDefault(t => t.TaskId == id);
+            if (taskToDelete != null)
+            {
+                // Remove target record
+                _repo.Tasks.Remove(taskToDelete);
+            }
+
+            // Grab remaining tasks from database
+            var tasks = _repo.Tasks.Where(x => x.Completed == 0).ToList();
+
+            // Send back to home with tasks included
+            return RedirectToAction("Index", tasks);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
